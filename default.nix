bbc484d7956c74b03d31749955dac31bc1e4e081
Travis Staton <hello@travisstaton.com>
2018-09-04 17:52:18 -0400

    change version

--- default.nix
+++ default.nix
@@ -9,17 +9,14 @@ let
     repo = "nixpkgs-channels";
     inherit (pinnedVersion) rev sha256;
   };
-  pinnedPython = hostPkgs.python36.withPackages
-        (pypkgs: with pypkgs; [
-        pytest
-        requests
-        ]);
 
 in with import pinnedPkgs { inherit config; };
 stdenv.mkDerivation rec {
   name = "env";
   env = buildEnv { name = name; paths = buildInputs; };
   buildInputs = [
-  pinnedPython
+  (python36.withPackages (pyPkgs: with pyPkgs; [
+  requests
+  ]))
   ];
 }
